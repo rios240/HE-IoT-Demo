ac11dc07cbf00c28519fccf997a8c66921d20ea7
Tyler <Tyler.Rios@du.edu>
2024-11-06 15:06:36 -0700

    Added DataStreamer, MachinerySensor, and wrote main

--- python/MachinerySensor/machinerysensor.py
+++ python/MachinerySensor/machinerysensor.py
@@ -4,37 +4,53 @@
 #       Vibration in mm/s
 #       Pressure in PSI
 
-import threading
+
+from threading import Thread, Event
 import time
 import random
 import errors
+import keying
+
 
 class MachinerySensor(threading.Thread):
     SLEEP_INTERVAL = 60
 
+    # Constructor
     def __init__(self, sensor_id, sensor_sn):
-        self.sensor_id = sensor_id
-        self.sensor_sn = sensor_sn
-        self.latest_number = None
+        super().__init__()
+        self.id = sensor_id
+        self.sn = sensor_sn
+        self.reading = None
         self.signal = threading.Event()
-        self.lock = threading.Lock()
-
-    def gen_number(self):
-        if self.sensor_sn == "T79HD20J":
+        self.signal.clear()
+        self.cert = None
+        self.key = None
+        if self.sn == "T79HD20J":
+            self.cert = keying.SENSOR_1A2B3C_CERTIFICATE
+            self.key = keying.SENSOR_1A2B3C_KEY
+        elif self.sn == "H54JU72D":
+            self.cert = keying.SENSOR_4D5E6F_CERTIFICATE
+            self.key = keying.SENSOR_4D5E6F_KEY
+        elif self.sn == "L20YT63C":
+            self.cert = keying.SENSOR_7G8H9I_CERTIFICATE
+            self.key = keying.SENSOR_7G8H9I_KEY
+        else:
+            raise errors.GenericError("Unknown sensor")
+            
+    # Generates a random sensor reading
+    def gen_reading(self):
+        if self.sn == "T79HD20J":
             return round(random.uniform(20.0, 100.0), 2)
-        elif self.sensor_sn == "H54JU72D":
+        elif self.sn == "H54JU72D":
             return round(random.uniform(0.0, 20.0), 2)
-        elif self.sensor_sn == "L20YT63C":
+        elif self.sn == "L20YT63C":
             return round(random.uniform(10.0, 100.0), 2)
         else:
             raise errors.GenericError("Unknown sensor")
 
+    # Function that runs when calling start() on object
     def run(self):
         while True:
-            number = self.gen_number()
-
-            with self.lock:
-                self.latest_number = number
-
+            self.reading = self.gen_reading()
             self.signal.set()
             time.sleep(self.SLEEP_INTERVAL)
